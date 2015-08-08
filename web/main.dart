d63a009252f63f5a2638d59926cfdd87b1e90a33
Andres Araujo <a.araujo.azua@gmail.com>
2015-08-08 14:46:23 -0500

    chore: update to alpha 34

--- web/main.dart
+++ web/main.dart
@@ -3,14 +3,7 @@ import 'package:angular2/bootstrap.dart';
 import 'package:angular2/di.dart';
 import 'package:angular2/router.dart';
 
-// These imports will go away soon:
-import 'package:angular2/src/reflection/reflection.dart' show reflector;
-import 'package:angular2/src/reflection/reflection_capabilities.dart'
-    show ReflectionCapabilities;
-
 void main() {
-  // Temporarily needed.
-  reflector.reflectionCapabilities = new ReflectionCapabilities();
 
   bootstrap(AppComp, [
   routerInjectables,
